ead88eeb40078de2497bb5cf020462dc258d55e9
Luiz Gustavo Pfitscher e Feldmann <lfeldmann@ael.com.br>
2022-11-03 11:28:05 +0200

    Automated generation of wxFormBuilder code from project as part of the build process

---
+++ ui/wxFormBuilder.cmake
@@ -0,0 +1,38 @@
+# Find the generator tool
+find_program(WXFORM wxFormBuilder.exe
+	"C:\\Program Files\\wxFormBuilder"
+	"C:\\Program Files (x86)\\wxFormBuilder"
+)
+
+# List of all UI projects to generate
+SET(WXFB_PROJECTS
+    "IMainWindow"
+    "INameDescEditor"
+    "IProjectEditor"
+    "IGroupEditor"
+    "IVariableEditor"
+    "ITextFileEditor"
+    "IRunProcessEditor"
+    "IOperationEditor"
+    "IConstantEditor"
+    "IConditionalEditor"
+)
+
+unset(WXFB_GENERATED)
+
+# Custom build for generated C++
+foreach (wxfbProj IN LISTS WXFB_PROJECTS)
+    SET(PrjFile "${CMAKE_CURRENT_LIST_DIR}/${wxfbProj}.fbp")
+    SET(SrcFile "${CMAKE_CURRENT_LIST_DIR}/wxExport/${wxfbProj}.cpp")
+    SET(IncFile "${CMAKE_CURRENT_LIST_DIR}/wxExport/${wxfbProj}.h")
+    
+    add_custom_command(
+      OUTPUT ${SrcFile}
+      OUTPUT ${IncFile}
+      COMMAND "${WXFORM}" -g "${PrjFile}"
+      DEPENDS ${PrjFile}
+      VERBATIM
+    )
+    
+    list(APPEND WXFB_GENERATED ${SrcFile} ${IncFile})
+endforeach()
